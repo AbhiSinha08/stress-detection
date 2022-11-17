dd2e49af757c24574418c030fbe5fb415f3d7e7b
Abhinav Sinha <imonline.abhinav@gmail.com>
2022-11-18 01:37:06 +0530

    Update Deploy Script

--- init.sh
+++ init.sh
@@ -4,10 +4,11 @@
 sudo -s
 
 apt update
-apt install -y python3 pip node npm
+apt install -y python3 pip nodejs npm
 git clone https://github.com/AbhiSinha08/stress-detection.git
 cd stress-detection
 pip install -r requirements.txt
+pip install jupyter numpy pandas scikit-learn flask flask_cors matplotlib seaborn
 
 cd client
 npm install --save-dev
